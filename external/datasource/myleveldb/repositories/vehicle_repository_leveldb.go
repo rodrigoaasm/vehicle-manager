37c08258713595507d905829d1b70896378c8d12
Rodrigo Maia <rodrigoasmaia@gmail.com>
2022-07-20 20:50:51 -0300

    Add leveldb vehicle repository

---
+++ external/datasource/myleveldb/repositories/vehicle_repository_leveldb.go
@@ -0,0 +1,81 @@
+package repositories
+
+import (
+	"demo/domain/entities"
+	"demo/domain/entities/abstract"
+	"encoding/json"
+	"errors"
+	"reflect"
+	"strings"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type VehicleRepository struct {
+	DB *leveldb.DB
+}
+
+func (repo VehicleRepository) transform(marshalledVehicle []byte) (abstract.IVehicle, error) {
+	payload := strings.Split(string(marshalledVehicle), "{")
+	if payload[0] == "*entities.Car" {
+		var vehicle entities.Car
+		errUnMarshal := json.Unmarshal([]byte("{"+payload[1]), &vehicle)
+		return &vehicle, errUnMarshal
+
+	} else if payload[0] == "*entities.Truck" {
+		var vehicle entities.Truck
+		errUnMarshal := json.Unmarshal([]byte("{"+payload[1]), &vehicle)
+		return &vehicle, errUnMarshal
+
+	}
+
+	return nil, errors.New("Invalid entity")
+}
+
+func (repo VehicleRepository) SaveVehicle(vehicle abstract.IVehicle) error {
+	marshalledVehicle, errMarshal := json.Marshal(vehicle)
+	if errMarshal != nil {
+		return errMarshal
+	}
+
+	// Add entity type to unmarshalled entity
+	objectType := reflect.TypeOf(vehicle).String()
+	marshalledData := append([]byte(objectType), marshalledVehicle...)
+
+	// Save
+	errStore := repo.DB.Put([]byte(vehicle.GetId()), marshalledData, nil)
+	if errStore != nil {
+		return errStore
+	}
+
+	return nil
+}
+
+func (repo VehicleRepository) GetAllVehicle() ([]abstract.IVehicle, error) {
+	iter := repo.DB.NewIterator(nil, nil)
+	vehicles := []abstract.IVehicle{}
+
+	for iter.Next() {
+		marshalledVehicle := iter.Value()
+		vehicle, err := repo.transform(marshalledVehicle)
+		if err != nil {
+			return nil, errors.New("Failed to transform one element")
+		}
+
+		vehicles = append(vehicles, vehicle)
+	}
+
+	if iter.Error() != nil {
+		return nil, errors.New("Failed to get data")
+	}
+
+	return vehicles, nil
+}
+
+func (repo VehicleRepository) GetVehicleById(id string) (abstract.IVehicle, error) {
+	marshalledVehicle, errGet := repo.DB.Get([]byte(id), nil)
+	if errGet != nil {
+		return nil, errGet
+	}
+	return repo.transform(marshalledVehicle)
+}
---
+++ external/datasource/myleveldb/repositories/vehicle_repository_leveldb_test.go
@@ -0,0 +1,45 @@
+package repositories_test
+
+import (
+	"demo/domain/entities"
+	"demo/domain/entities/abstract"
+	"demo/external/datasource/myleveldb"
+	"demo/external/datasource/myleveldb/repositories"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var db, errDatabase = myleveldb.CreateLevelDB("test/data")
+var vehicleRepository = repositories.VehicleRepository{DB: db}
+
+var car = entities.NewCar("a154", "VW GOL", "black", "14885511T125T")
+var truck = entities.NewTrunk("a156", "VW CONSTELLATION", "white", "2885511T125E")
+
+func TestCreateLevelDB(t *testing.T) {
+	require.Nil(t, errDatabase, "should create a local database")
+}
+
+func TestSaveVehicle(t *testing.T) {
+	err := vehicleRepository.SaveVehicle(car)
+	require.Nil(t, err, "should save the Car")
+
+	err = vehicleRepository.SaveVehicle(truck)
+	require.Nil(t, err, "should save the Truck")
+}
+
+func TestGetVehicleById(t *testing.T) {
+	result, _ := vehicleRepository.GetVehicleById("a154")
+	assert.Equal(t, car, result, "should return the Car with id 'a154'")
+
+	result, _ = vehicleRepository.GetVehicleById("a156")
+	assert.Equal(t, truck, result, "should return the Truck with id 'a156'")
+}
+
+func TestGetAllVehicleById(t *testing.T) {
+	result, _ := vehicleRepository.GetAllVehicle()
+	vehicles := []abstract.IVehicle{car, truck}
+
+	assert.Equal(t, vehicles, result, "should return all vehicles")
+}
